f4280a716aed1a5fba8b9c077c7457229e32ca51
mtnleo <mtnleonardi@gmail.com>
2022-12-23 23:20:52 -0300

    Succesfully implemented and partially troubleshooted adding a background image to the png headshot image. Still some things to change

--- edited_image.py
+++ edited_image.py
@@ -17,6 +17,8 @@ class edited_image:
     def remove_background(self):
         removed_img = remove(self.image)
         removed_img.save(self.folder_handler.get_edited_pictures_folder() + self.folder_handler.get_output_name_new_image())
+
+        return removed_img
         
     def get_all_pictures_from_folder(path=os.getcwd()):
         new_fh = fh.FolderHandling(path)
@@ -35,7 +37,47 @@ class edited_image:
 
         return random_image_path
 
+    def _resize_images(self, background):
+        headshot_size = self.image.size
+        background_size = background.size
+
+        new_size = [0, 0]
+        if headshot_size[0] < background_size[0]:
+            new_size[0] = headshot_size[0]
+        else:
+            new_size[0] = background_size[0]
+        
+        if headshot_size[1] < background_size[1]:
+            new_size[1] = headshot_size[1]
+        else:
+            new_size[1] = background_size[1]
+
+        resized_headshot = self.image.resize(size=tuple(new_size))
+        resized_background = background.resize(size=tuple(new_size))
+
+        return resized_headshot, resized_background
+
+
+    def _merge_headshot_and_background(self):
+        ## try 1
+        self.image = self.remove_background()
+        ##
+
+        background_path = self._get_background_image_path()
+        background = Image.open(background_path)
+
+        resized_headshot, resized_background = self._resize_images(background)
+        resized_headshot = resized_headshot.convert(mode="RGBA")
+        resized_background = resized_background.convert(mode="RGBA")
+
+        # merging background + headshot image
+        merged_image = Image.alpha_composite(resized_background, resized_headshot)
+
+        merged_image.save(self.folder_handler.get_edited_pictures_folder() + self.folder_handler.get_output_name_new_image())
+
+        return merged_image
+
 
 if __name__ == "__main__":
-    im = edited_image(Image.open("D:\\Programming\\Python\\Image Processing\\unedited_images\\download.jpeg"))
-    print(im._get_background_image_path())
+    im = edited_image(Image.open("D:\\Programming\\Python\\Image Processing\\unedited_images\\Professional Headshot.png"))
+    im._merge_headshot_and_background()
